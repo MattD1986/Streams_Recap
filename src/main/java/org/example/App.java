a32130b5c3fe8bf6ca129116a480eacf111e1879
Matthew Desramault <matthew.desramault@student.vives.be>
2022-04-18 15:15:47 +0200

    solution

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 
 public class App {
 
@@ -19,35 +23,55 @@ public class App {
 
     //Filter books with pages >400
     System.out.println("---All books over 400 pages ---");
-    //without foreach in stream operation
     //code here
 
 
     //with foreach in stream operation
     //code here
-
+    bookList.stream()
+            .filter(book -> book.getPages() > 400)
+            .forEach(System.out::println);
 
     System.out.println("---All unique items---");
     //code here
+    bookList.stream()
+            .distinct()
+            .forEach(System.out::println);
 
 
     System.out.println("---All books of authors with Last Name starting with 'C', ordered on last name---");
-    //code here
+    //weergave van de lijst van boeken van auteurs met familienaam die start met een "C", gesorteerd op familienaam
+    bookList.stream()
+            .filter(book -> book.getAuthorLastName().startsWith("C"))
+            .sorted(comparing(book -> book.getAuthorLastName()))
+            .forEach(System.out::println);
 
     System.out.println("---All books of authors with Last Name starting with 'C', ordered on book title---");
-    //code here
+    //idem, maar gesorteerd op boektitel
+    bookList.stream()
+            .filter(book -> book.getAuthorLastName().startsWith("C"))
+            .sorted(comparing(book -> book.getTitle()))
+            .forEach(System.out::println);
 
 
     System.out.println("--Amount of books where author first name equals 3 characters");
-    //code here
+    List<Book> amountOfBooks = bookList.stream()
+            .filter(book -> book.getAuthorFirstName().length() == 3)
+            .collect(toList());
+    System.out.println(amountOfBooks.stream().count());
 
-    System.out.println("--Total amount of pages of all books with author Last Name starting with 'S'--");
-    //code here
+    System.out.println("--Total amount of pages of all books with author Last Name starting with 'S'");
+    System.out.println(bookList.stream()
+            .filter(book -> book.getAuthorLastName().startsWith("S"))
+            .map(Book::getPages)          //gaat van type book naar type Integer via de methode aanroep)
+            .reduce(0,(total,book)->total+book));
 
 
     System.out.println("--Display the first book that contains 300-400 pages with a character 'o' in it's title");
-    //code here
-
-
+    bookList.stream()
+            .filter(book -> book.getPages() > 300 && book.getPages() < 400)
+            .filter(book -> book.getTitle().contains("o"))
+            .limit(1)
+            .forEach(System.out::println);
   }
 }
